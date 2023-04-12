84d134d2ab62defa504bd4ce7dedd58f7ecfb55a
thigo47 <vilaportilho@gmail.com>
2023-04-12 21:56:34 +0100

    att

---
+++ script.js
@@ -0,0 +1,6 @@
+let menus = document.getElementsByClassName("menus")
+let teste = document.getElementById("teste")
+function clicar(){
+    menus.style.display = "block";
+    teste.style.display = "block"
+}
